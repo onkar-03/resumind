3744563f0fc534d948836937e6afa87c342de31f
Onkar Patel <onkarpatel10@gmail.com>
2025-07-27 23:07:28 +0530

    Wipe dummy data & fix back to homepage navigation

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -67,12 +67,18 @@ const Resume = () => {
   return (
     <main className='!pt-0'>
       <nav className='resume-nav'>
-        <Link to='/' className='back-button'>
+        <button
+          onClick={() => {
+            console.log('Back button clicked - forcing page navigation');
+            window.location.href = '/';
+          }}
+          className='back-button'
+        >
           <img src='/icons/back.svg' alt='logo' className='w-2.5 h-2.5' />
           <span className='text-gray-800 text-sm font-semibold'>
             Back to Homepage
           </span>
-        </Link>
+        </button>
       </nav>
       <div className='flex flex-row w-full max-lg:flex-col-reverse'>
         <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-center">

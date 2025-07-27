9cbc8506aa6de69df1be6868d5cb383161c52593
Onkar Patel <onkarpatel10@gmail.com>
2025-07-27 16:17:30 +0530

    feat: complete Home UI with Navbar and ResumeCard

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import ResumeCard from '~/components/ResumeCard';
 import { resumes } from '../../constants';
 import type { Route } from './+types/home';
 import Navbar from '~/components/Navbar';
@@ -18,12 +19,14 @@ export default function Home() {
           <h1>Track Your Applications & Resume Ratings</h1>
           <h2>Review your submissions and check AI-powered feedback.</h2>
         </div>
+        {resumes.length > 0 && (
+          <div className='resumes-section'>
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
       </section>
-      {resumes.map((resume) => (
-        <div key={resume.id}>
-          <h1>{resume.jobTitle}</h1>
-        </div>
-      ))}
     </main>
   );
 }
